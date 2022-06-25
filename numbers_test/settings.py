0e879e66397df6425bd398b8d3d3495e2cd1871e
cybertoad <cyber704d@gmail.com>
2022-06-25 10:45:53 +0300

    switching to postgresql

--- numbers_test/settings.py
+++ numbers_test/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'numbers_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'numbers',
+        'USER': 'numbers',
+        'PASSWORD': 'numbers',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
